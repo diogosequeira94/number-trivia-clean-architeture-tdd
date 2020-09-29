2eb1240481f328bb2e8e12d45347fcab6fd11d95
Diogo Sequeira <CTW01015@criticaltechworks.com>
2020-09-29 11:15:02 +0100

    exceptions class

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -3,3 +3,15 @@ import 'package:equatable/equatable.dart';
 abstract class Failure extends Equatable {
   Failure([List properties = const<dynamic>[]]);
 }
+
+// General Failures
+
+class ServerException extends Failure {
+  @override
+  List<Object> get props =>[];
+}
+
+class CacheException extends Failure{
+  @override
+  List<Object> get props => [];
+}
--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,9 +1,8 @@
 import 'package:clean_architecture_tdd_course/core/error/failures.dart';
 import 'package:clean_architecture_tdd_course/core/usecases/usecase.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
-import '../repositories/number_trivia_repository.dart';
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
+import '../repositories/number_trivia_repository.dart';
 
 class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
 
